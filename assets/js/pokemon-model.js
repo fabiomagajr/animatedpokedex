33b5bc4580171a9ea71b4bc1a9ca680095b40b41
Fabio Magalhães <112334507+fabiomagajr@users.noreply.github.com>
2024-04-09 07:35:50 -0300

    Create pokemon-model.js

---
+++ assets/js/pokemon-model.js
@@ -0,0 +1,22 @@
+
+class Pokemon {
+    number;
+    name;
+    type;
+    types = [];
+    photo;
+    backphoto;
+    frontphoto;
+    stats=[];
+    stat;
+    hp;
+    atk;
+    def;
+    sp_atk;
+    sp_def;
+    speed;
+
+
+    abilities=[];
+    ability;
+}
